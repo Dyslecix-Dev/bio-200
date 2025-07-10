5e85a79462c6658cc9dbf8225f2799d43d861c24
Christian Demesa <dyslecixdev@gmail.com>
2025-07-09 23:07:43 -0700

    Added topics 8 and 9 exam questions

--- app/_data/exams/exam-two.ts
+++ app/_data/exams/exam-two.ts
@@ -83,6 +83,161 @@ export const trueOrFalseQuestions = [
       { text: "False", correct: false },
     ],
   },
+
+  {
+    question: "The accessory digestive organs aid in mechanically or chemically breaking down digested food.",
+    options: [
+      { text: "True", correct: true },
+      { text: "False", correct: false },
+    ],
+  },
+  {
+    question: "Most digestive organs are in the abdominal cavity.",
+    options: [
+      { text: "True", correct: false },
+      { text: "False", correct: true },
+    ],
+  },
+  {
+    question: "The central abdominal regions contain most of the large and small intestines.",
+    options: [
+      { text: "True", correct: true },
+      { text: "False", correct: false },
+    ],
+  },
+  {
+    question: "The visceral peritoneum lines the inner abdominopelvic walls and loosely wraps around the digestive organs.",
+    options: [
+      { text: "True", correct: false },
+      { text: "False", correct: true },
+    ],
+  },
+  {
+    question: "Propulsion, the movement of food through the alimentary canal, includes swallowing and peristalsis.",
+    options: [
+      { text: "True", correct: true },
+      { text: "False", correct: false },
+    ],
+  },
+  {
+    question: "Chemical digestion occurs in the mouth by mixing food with saliva, which contains enzymes.",
+    options: [
+      { text: "True", correct: true },
+      { text: "False", correct: false },
+    ],
+  },
+  {
+    question: "The bulk of a tooth is made of dentin, a spongy type of bone more similar to skeletal bone.",
+    options: [
+      { text: "True", correct: false },
+      { text: "False", correct: true },
+    ],
+  },
+  {
+    question: "The muscularis externa consists of two layers of skeletal muscle.",
+    options: [
+      { text: "True", correct: false },
+      { text: "False", correct: true },
+    ],
+  },
+  {
+    question: "The visceral peritoneum is the outermost lining of the alimentary canal.",
+    options: [
+      { text: "True", correct: true },
+      { text: "False", correct: false },
+    ],
+  },
+  {
+    question: "The esophagus is a continuation of the larynx.",
+    options: [
+      { text: "True", correct: false },
+      { text: "False", correct: true },
+    ],
+  },
+  {
+    question: "The lining epithelium of the stomach mucosa secretes a special type of buffered mucus that protects the stomach from pepsin and hydrochloric acid.",
+    options: [
+      { text: "True", correct: true },
+      { text: "False", correct: false },
+    ],
+  },
+  {
+    question: "The large intestine is the longest part of the alimentary canal, and is the site of most enzymatic digestion.",
+    options: [
+      { text: "True", correct: false },
+      { text: "False", correct: true },
+    ],
+  },
+  {
+    question: "The small intestine is where some absorption of nutrients occur.",
+    options: [
+      { text: "True", correct: false },
+      { text: "False", correct: true },
+    ],
+  },
+  {
+    question: "By the time chyme reaches the large intestine, it has few nutrients left and is mostly indigestible waste.",
+    options: [
+      { text: "True", correct: true },
+      { text: "False", correct: false },
+    ],
+  },
+  {
+    question: "The liver is the smallest gland in the body.",
+    options: [
+      { text: "True", correct: false },
+      { text: "False", correct: true },
+    ],
+  },
+  {
+    question: "Hepatocytes have a high capacity for cell division.",
+    options: [
+      { text: "True", correct: true },
+      { text: "False", correct: false },
+    ],
+  },
+  {
+    question: "The kidneys lie below the parietal peritoneum, specifically below the liver and stomach.",
+    options: [
+      { text: "True", correct: false },
+      { text: "False", correct: true },
+    ],
+  },
+  {
+    question: "Calices are where urine produced by kidney tissues is deposited.",
+    options: [
+      { text: "True", correct: true },
+      { text: "False", correct: false },
+    ],
+  },
+  {
+    question: "Major calices are formed by adjacent renal columns, and drain into minor calices, which drain into the renal pelvis.",
+    options: [
+      { text: "True", correct: false },
+      { text: "False", correct: true },
+    ],
+  },
+  {
+    question: "Nephrons and their convoluted tubules are entwined with peritubular capillaries.",
+    options: [
+      { text: "True", correct: true },
+      { text: "False", correct: false },
+    ],
+  },
+  {
+    question: "Juxtamedullary nephrons produce the bulk of urine, and cortical nephrons contribute to concentrating the urine product.",
+    options: [
+      { text: "True", correct: false },
+      { text: "False", correct: true },
+    ],
+  },
+  {
+    question: "The ureters transport urine actively through peristalsis.",
+    options: [
+      { text: "True", correct: true },
+      { text: "False", correct: false },
+    ],
+  },
 ];
 
 export const multipleChoiceQuestions = [
@@ -93,7 +248,7 @@ export const multipleChoiceQuestions = [
       { text: "Pulmonary ventilation", correct: false },
       { text: "Transport of respiratory gases", correct: false },
       { text: "Internal respiration", correct: false },
-      { text: "All of the above", correct: false },
+      { text: "All of the given options", correct: false },
     ],
   },
   {
@@ -103,7 +258,7 @@ export const multipleChoiceQuestions = [
       { text: "External respiration", correct: false },
       { text: "Pulmonary ventilation", correct: false },
       { text: "Internal respiration", correct: false },
-      { text: "None of the above", correct: false },
+      { text: "None of the given options", correct: false },
     ],
   },
   {
@@ -133,7 +288,7 @@ export const multipleChoiceQuestions = [
       { text: "Mucus associated lymphoid tissue", correct: false },
       { text: "Mucin associated lymphoid tissue", correct: false },
       { text: "Mucina associated lymphoid tissue", correct: false },
-      { text: "None of the above", correct: false },
+      { text: "None of the given options", correct: false },
     ],
   },
   {
@@ -149,7 +304,7 @@ export const multipleChoiceQuestions = [
   {
     question: "Which of the following is not a function of the larynx?",
     options: [
-      { text: "None of the above", correct: true },
+      { text: "None of the given options", correct: true },
       { text: "Providing an open airway", correct: false },
       { text: "Producing vocalizations", correct: false },
       { text: "Switching mechanism for food", correct: false },
@@ -163,7 +318,7 @@ export const multipleChoiceQuestions = [
       { text: "Pharynx", correct: false },
       { text: "Nasal cavity", correct: false },
       { text: "Oral cavity", correct: false },
-      { text: "None of the above", correct: false },
+      { text: "None of the given options", correct: false },
     ],
   },
   {
@@ -174,6 +329,256 @@ export const multipleChoiceQuestions = [
       { text: "Lungs", correct: false },
       { text: "Bronchial trees", correct: false },
       { text: "Alveolar sacs", correct: false },
+    ],
+  },
+  {
+    question: "Which of the following is not a structure of the alimentary canal?",
+    options: [
+      { text: "Trachea", correct: true },
+      { text: "Pharynx", correct: false },
+      { text: "Esophagus", correct: false },
+      { text: "Small intestines", correct: false },
+      { text: "Large intestines", correct: false },
+    ],
+  },
+  {
+    question: "Which of the following is not a function of the alimentary canal?",
+    options: [
+      { text: "Chewing food", correct: true },
+      { text: "Propelling food through the body", correct: false },
+      { text: "Digesting nutrients", correct: false },
+      { text: "Absorbing nutrients", correct: false },
+      { text: "Expelling waste", correct: false },
+    ],
+  },
+  {
+    question: "Which of the following is not an accessory digestive organ?",
+    options: [
+      { text: "Stomach", correct: true },
+      { text: "Tongue", correct: false },
+      { text: "Salivary glands", correct: false },
+      { text: "Gallbladder", correct: false },
+      { text: "Pancreas", correct: false },
+    ],
+  },
+  {
+    question: "In abdominal regions, the lower horizontal line connects what?",
+    options: [
+      { text: "Widest points of the iliac crests", correct: true },
+      { text: "Inferior points of the costal margins", correct: false },
+      { text: "Midpoint of the clavicles", correct: false },
+      { text: "Fusion of the pubic symphysis", correct: false },
+      { text: "None of the given options", correct: false },
+    ],
+  },
+  {
+    question: "Which of the following is not an organ in the inferior three abdominal regions?",
+    options: [
+      { text: "Liver", correct: true },
+      { text: "Cecum", correct: false },
+      { text: "Appendix", correct: false },
+      { text: "Urinary bladder", correct: false },
+      { text: "Anus", correct: false },
+    ],
+  },
+  {
+    question: "Which of the following is the largest mesentary?",
+    options: [
+      { text: "Greater omentum", correct: true },
+      { text: "Superior omentum", correct: false },
+      { text: "Main omentum", correct: false },
+      { text: "Vast omentum", correct: false },
+      { text: "Massive omentum", correct: false },
+    ],
+  },
+  {
+    question: "Which of the following is not an essential food-processing activity of the digestive system?",
+    options: [
+      { text: "Masticulation", correct: true },
+      { text: "Propulsion", correct: false },
+      { text: "Mechanical breakdown", correct: false },
+      { text: "Absorption", correct: false },
+      { text: "Defecation", correct: false },
+    ],
+  },
+  {
+    question: "How many permanent teeth is a full set?",
+    options: [
+      { text: "32", correct: true },
+      { text: "30", correct: false },
+      { text: "34", correct: false },
+      { text: "36", correct: false },
+      { text: "38", correct: false },
+    ],
+  },
+  {
+    question: "Which of the following is not a classification of teeth?",
+    options: [
+      { text: "Lateral", correct: true },
+      { text: "Incisor", correct: false },
+      { text: "Canine", correct: false },
+      { text: "Bicuspid", correct: false },
+      { text: "Molar", correct: false },
+    ],
+  },
+  {
+    question: "Which of the following is not a function of saliva?",
+    options: [
+      { text: "Mechanical breakdown of food", correct: true },
+      { text: "Moistens the mouth", correct: false },
+      { text: "Dissolves food chemicals", correct: false },
+      { text: "Binds food into a bolus", correct: false },
+      { text: "Kills foreign bacteria", correct: false },
+    ],
+  },
+  {
+    question: "Which of the following is not a tissue of the walls of the alimentary canal?",
+    options: [
+      { text: "Muscularis interna", correct: true },
+      { text: "Mucosa", correct: false },
+      { text: "Submucosa", correct: false },
+      { text: "Muscularis externa", correct: false },
+      { text: "Serosa", correct: false },
+    ],
+  },
+  {
+    question: "Which enzyme chemically breaks down food proteins within the stomach?",
+    options: [
+      { text: "Pepsin", correct: true },
+      { text: "Bismisin", correct: false },
+      { text: "Tumsin", correct: false },
+      { text: "Probiosin", correct: false },
+      { text: "Cocasin", correct: false },
+    ],
+  },
+  {
+    question: "Which of the following is not a region of the small intestine?",
+    options: [
+      { text: "Avarium", correct: true },
+      { text: "Duodenum", correct: false },
+      { text: "Jejunum", correct: false },
+      { text: "Ileum", correct: false },
+      { text: "None of the given options", correct: false },
+    ],
+  },
+  {
+    question: "Which of the following is not a structural modification of the inner wall of the small intestine?",
+    options: [
+      { text: "Cilia", correct: true },
+      { text: "Circular folds", correct: false },
+      { text: "Villi", correct: false },
+      { text: "Microvilli", correct: false },
+      { text: "None of the given options", correct: false },
+    ],
+  },
+  {
+    question: "Which of the following is not a functional example of the liver?",
+    options: [
+      { text: "Converting glycogen into glucose", correct: true },
+      { text: "Processing amino acids", correct: false },
+      { text: "Storing certain vitamins", correct: false },
+      { text: "Detoxifying poisons", correct: false },
+      { text: "Producing blood proteins", correct: false },
+    ],
+  },
+  {
+    question: "Which of the following is not part of the urinary system?",
+    options: [
+      { text: "None of the given options", correct: true },
+      { text: "Kidneys", correct: false },
+      { text: "Ureters", correct: false },
+      { text: "Urinary bladder", correct: false },
+      { text: "Urethra", correct: false },
+    ],
+  },
+  {
+    question: "Which of the following is not a nitrogenous compound excreted by urine?",
+    options: [
+      { text: "Carnitine", correct: true },
+      { text: "Urea", correct: false },
+      { text: "Uric acid", correct: false },
+      { text: "Creatinine", correct: false },
+      { text: "None of the given options", correct: false },
+    ],
+  },
+  {
+    question: "Where does uric acid come from?",
+    options: [
+      { text: "Replacing nucleic acids", correct: true },
+      { text: "Breakdown of proteins", correct: false },
+      { text: "Breakdown of muscle energy storage molecules", correct: false },
+      { text: "Replacing lipids", correct: false },
+      { text: "Breakdown of amino acids", correct: false },
+    ],
+  },
+  {
+    question: "Which is a distinct region of kidney tissue?",
+    options: [
+      { text: "Renal medulla", correct: true },
+      { text: "Renal endosteo", correct: false },
+      { text: "Renal epiphysia", correct: false },
+      { text: "Renal diaphysia", correct: false },
+      { text: "Renal periosteo", correct: false },
+    ],
+  },
+  {
+    question: "Extensions of the renal cortex dip down between what to form renal columns?",
+    options: [
+      { text: "Renal pyramids", correct: true },
+      { text: "Renal cuboids", correct: false },
+      { text: "Renal columnars", correct: false },
+      { text: "Renal trapezoids", correct: false },
+      { text: "Renal cones", correct: false },
+    ],
+  },
+  {
+    question: "At any given moment, both kidneys hold more than what percent of the body's total blood volume?",
+    options: [
+      { text: "20%", correct: true },
+      { text: "10%", correct: false },
+      { text: "15%", correct: false },
+      { text: "25%", correct: false },
+      { text: "30%", correct: false },
+    ],
+  },
+  {
+    question: "Which of the following are found in the medulla of nephrons?",
+    options: [
+      { text: "Nephron loop", correct: true },
+      { text: "Renal corpuscles", correct: false },
+      { text: "Proximal convoluted tubules", correct: false },
+      { text: "Distal convoluted tubules", correct: false },
+      { text: "Glomerulus", correct: false },
+    ],
+  },
+  {
+    question: "Which of the following leak blood plasma?",
+    options: [
+      { text: "Glomerulus", correct: true },
+      { text: "Nephron loop", correct: false },
+      { text: "Renal corpuscles", correct: false },
+      { text: "Proximal convoluted tubules", correct: false },
+      { text: "Distal convoluted tubules", correct: false },
+    ],
+  },
+  {
+    question: "Which of the following is not a renal process nephron performs?",
+    options: [
+      { text: "Renal diffusion", correct: true },
+      { text: "Glomerular filtration", correct: false },
+      { text: "Tubular resorption", correct: false },
+      { text: "Tubular secretion", correct: false },
+      { text: "None of the given options", correct: false },
+    ],
+  },
+  {
+    question: "The urinary bladder is posterior to which structure?",
+    options: [
+      { text: "Pubic symphysis", correct: true },
+      { text: "Inferior pelvic region", correct: false },
+      { text: "Rectum", correct: false },
+      { text: "Urethra", correct: false },
+      { text: "Kidneys", correct: false },
     ],
   },
 ];
@@ -199,5 +604,53 @@ export const shortAnswerQuestions = [
     question: "How does inspiration and expiration work mechanistically?",
     answer:
       "When the diaphragm contracts, the lungs expand, causing the air pressure to decrease and draw in air from the external environment. When the diaphragm relaxes, the lungs shrink, causing the air pressure to increase and expel air into the external environment.",
+  },
+  {
+    question: "What are the two main groups of the digestive system?",
+    answer: "Alimentary canal (i.e. gastrointestinal tract) organs, and accessory digestive organs.",
+  },
+  {
+    question: "What are the 3 functions of mesentaries?",
+    answer: "Hold organs in place, store fat, and provide scaffolding for circulatory vessels and nerves to reach organs.",
+  },
+  {
+    question: "What are the 2 main regions of teeth?",
+    answer: "An exposed crown, and hidden roots in the socket.",
+  },
+  {
+    question: "What is the function of the epithelium lining of the lumen (part of the mucosa) in small intestines?",
+    answer: "It absorbs nutrients and passes them to the blood vessels of the lamina propria.",
+  },
+  {
+    question: "What are 2 differences between the pharynx and the esophagus?",
+    answer: "The esophagus has a collapsed lumen, and its walls contain all four tissue layers of the alimentary canal.",
+  },
+  {
+    question: "Most digestive organs are secreted into the duodenum from which 2 organs?",
+    answer: "Pancreas and liver.",
+  },
+  {
+    question: "What does the gallbladder do?",
+    answer: "It stores and concentrates bile from the liver.",
+  },
+  {
+    question: "What is the function of the kidneys?",
+    answer: "They maintain the chemical constancy of the blood and extracellular body fluids.",
+  },
+  {
+    question: "Which 2 things hold the kidneys in place?",
+    answer: "Surrounding layers of perirenal and pararenal fat, and renal fascia.",
+  },
+  {
+    question: "Why are the kidneys heavily vascularized?",
+    answer: "Because they filter the body's blood.",
+  },
+  {
+    question: "What are the 2 classes of nephrons?",
+    answer: "Cortical nephrons, and juxtamedullary nephrons.",
+  },
+  {
+    question: "Why do the ureters actively transport urine?",
+    answer: "It prevents urine from flowing back to the kidneys when the body is horizontal or upside-down, or when the urinary bladder is full.",
   },
 ];
